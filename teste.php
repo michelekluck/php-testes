7330767c60b1cb3390bf145af1929e36584355a2
michelekluck <michelenamur27@gmail.com>
2024-11-07 23:41:51 -0300

    print

---
+++ teste.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de PHP</title>
+    <style>
+        body {
+            background-color: lightsteelblue;
+        }
+
+        p {
+            font-family: verdana;
+            color: white;
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        echo"<h2>Olá mundo!</h2>";
+        print "<p>Olá de novo, mundo!</p>"
+    ?>
+</body>
+</html>
