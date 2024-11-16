7d16c79422c1164332ab1b20acc3d0463697471d
michelekluck <michelenamur27@gmail.com>
2024-11-16 12:06:40 -0300

    variaveis e echo

---
+++ echoAndPrint.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <?php
+        $resultado = print "Olá mundo!";
+        echo $resultado;
+
+        echo "<h2>PHP is fun!</h2>";
+        echo "Hello world! <br>";
+        echo "I'm about to learn PHP! <br>";
+        echo "This ", "string", "was", "made", "with", "multiple parameters.";
+
+        $txt1 = "Learn PHP";
+        $txt2 = "W3School.com";
+
+        echo "<h2>$txt1</h2>";
+        echo "<p>Study PHP at $txt2</p>";
+        ?>
+    </body>
+</html>
---
+++ variaveis/varStatic.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <?php
+            function myTest() {
+                static $x = 0;
+                echo $x;
+                $x++;
+            }
+
+            myTest();
+            echo "<br>";
+            myTest();
+            echo "<br>";
+            myTest();
+            echo "<br>";
+            myTest();
+        ?>
+    </body>
+</html>
